c2c5c541694976144a91ecf40761f0d4e61396f2
Balpreet2001 <balpreetsingh8845@gmail.com>
2023-04-19 04:41:39 +0530

    Lifted up state to app component.Show status message

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
 import { useState } from 'react'
 import './styles.scss'
 import { Board } from './components/Board'
+import { calculateWinner } from './winner';
 
 function App() {
-  
-  const [counter, setCounter] = useState(1)
 
-  console.log("hello")
+  const [sqaures , setSquares] = useState(Array(9).fill(null));
+  const [isXNext , setIsXNext] = useState(false)
 
-  const onBtnclick =() =>{
-        setCounter(currentCounter =>{
-          return currentCounter +1
+
+  const winner = calculateWinner(sqaures)
+  const nextPlayer = isXNext ? 'X' : 'O'
+  const statusMessage = winner ? `Winner is ${winner}` : `Next player is ${nextPlayer}`
+
+  const handleSquareClick = clickedPosition => {
+    if(sqaures[clickedPosition] || winner ){
+      return
+    }
+    
+    setSquares(currentSquares => {
+        return currentSquares.map((squareValue , position)=>{
+          if(clickedPosition === position){
+            return isXNext ? 'X' : 'O'
+          }
+
+          return squareValue;
         })
+    })
+
+    setIsXNext((currentIsXNext) => !currentIsXNext)
   }
 
   return (
     <div className="app">
+      <div><h1> { statusMessage } </h1></div>
        
-        <Board />
-
-        <div>
-          <button onClick={onBtnclick}>
-            I am a clicky button
-          </button>
-          <div>{counter}</div>
-        </div>
+        <Board sqaures = {sqaures} handleSquareClick={handleSquareClick} />
       
     </div>
     
